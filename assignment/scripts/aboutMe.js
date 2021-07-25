ceb92811048c7acd52ab25b8975c916407df13cc
Phaydara Vongsavanthong <stephen.mussel@gmail.com>
2021-07-25 09:27:44 -0500

    double-check and ran thru console.log up to #6

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -1,27 +1,32 @@
 // REQUIRED FEATURES:
 // 1 - Create a variable called `firstName` and assign it the value of your first name
 let firstName = 'Phaydara ';
+console.log(firstName);
 
 // 2 - Create a second variable called `lastName` and assign it the value of your last name
 let lastName = 'Vongsavanthong';
+console.log(lastName);
 
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 // (remember, you already have variables for this, can you use those?)
 let fullName = firstName + lastName;
+console.log(fullName);
 
 // 4 - Console log the value of `fullName`
 console.log(fullName);
 
 // 5 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
 let luckyNumber = 5;
+console.log(luckyNumber);
 
 // 6 - Console log this sentence, adding in the variables you created above:
 // 'My name is (full name), and I think (lucky number) is a winner!'.
 // Refer back to the videos if you need help with this one.
 console.log('My name is', fullName + ', and I think', luckyNumber, 'is a winner!' );
+console.log(`My name is ${fullName}, and I think ${luckyNumber} is a winner`);
 
 // 7 - Create a variable named `adventurous` and set it to a boolean value (true or false)
-let adventurous = true;
+/* let adventurous = true;
 
 // 8 - Create a variable named `food`, and set its value to a string of your favorite food
 let food = 'mangos';
@@ -83,3 +88,4 @@ if (mostPets) {
 
 // 18 -- Rewrite question 13 with a `ternary` operator. You'll need to do research!
 adventurous ? console.log('Adventures are great!'): console.log('How about we stay home?');
+*/
